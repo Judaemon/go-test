d3ad8cf1d2114bc297ba22f4854acdd766ab6c14
John ray Judaya <jrj@newmediastaff.com>
2024-03-14 15:25:47 +0800

    Add values() function to main.go

--- main.go
+++ main.go
@@ -6,8 +6,21 @@ import (
 
 func main() {
     helloWorld()
+
+    values()
 }
 
 func helloWorld()  {
     fmt.Println("hello world")
+}
+
+func values()  {
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
 }
