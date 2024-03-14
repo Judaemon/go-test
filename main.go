560408b8271336a25a2686fa488eeab844280c18
John ray Judaya <jrj@newmediastaff.com>
2024-03-14 15:28:21 +0800

    Add variables function

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main() {
     helloWorld()
 
     values()
+
+    variables()
 }
 
 func helloWorld()  {
@@ -23,4 +25,21 @@ func values()  {
     fmt.Println(true && false)
     fmt.Println(true || false)
     fmt.Println(!true)
+}
+
+func variables()  {
+    var a = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "apple"
+    fmt.Println(f)
 }
