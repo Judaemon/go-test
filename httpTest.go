2bf5be0212aa4c4636c54b8bb843fc2be09f3aae
John ray Judaya <jrj@newmediastaff.com>
2024-03-14 15:23:32 +0800

    Refactor code: Delete hello-world.go and add httpTest.go and helloWorld() function

---
+++ httpTest.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func httpTest() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, from test")
+    })
+
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    
+    http.ListenAndServe(":8081", nil)
+}
--- main.go
+++ main.go
@@ -2,20 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+    helloWorld()
+}
 
-    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, from test")
-    })
-
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-    
-    http.ListenAndServe(":8081", nil)
+func helloWorld()  {
+    fmt.Println("hello world")
 }
