e9671e5b50bbdd998b38b2510f651412e0b0cdb2
John ray Judaya <jrj@newmediastaff.com>
2024-03-14 14:00:43 +0800

    Add variable declarations and print statements

--- hello-world.go
+++ hello-world.go
@@ -13,4 +13,23 @@ func main() {
     fmt.Println(true && false)
     fmt.Println(true || false)
     fmt.Println(!true)
+
+	var a = "initial"
+    fmt.Println(a)
+
+	// multiple variables can be declared at once
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+	// Go will infer the type of initialized variables
+    var isHappy = true
+    fmt.Println(isHappy)
+
+	// Variables declared without a corresponding initialization are zero-valued.
+    var score int
+    fmt.Println(score)
+
+	// The := syntax is shorthand for declaring and initializing a variable
+    var fruit = string("apple")
+    fmt.Println(fruit)
 }
