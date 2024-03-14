2a0486660848876a1167da5d32dd5e2198063a07
John ray Judaya <jrj@newmediastaff.com>
2024-03-14 15:33:49 +0800

    Add ifelse() function for conditional statements

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
     constant()
 
     forLoop()
+
+    ifelse()
 }
 
 func helloWorld()  {
@@ -88,5 +90,29 @@ func forLoop()  {
             continue
         }
         fmt.Println(n)
+    }
+}
+
+func ifelse()  {
+    if 7%2 == 0 {
+        fmt.Println("7 is even")
+    } else {
+        fmt.Println("7 is odd")
+    }
+
+    if 8%4 == 0 {
+        fmt.Println("8 is divisible by 4")
+    }
+
+    if 8%2 == 0 || 7%2 == 0 {
+        fmt.Println("either 8 or 7 are even")
+    }
+
+    if num := 9; num < 0 {
+        fmt.Println(num, "is negative")
+    } else if num < 10 {
+        fmt.Println(num, "has 1 digit")
+    } else {
+        fmt.Println(num, "has multiple digits")
     }
 }
